086c551294e91677f721959064f1ac42edb3cb04
Thura Aung <thuraaung1601mandalay@gmail.com>
2020-10-20 14:36:43 +0630

    Add files via upload

---
+++ Day6/iterobj.py
@@ -0,0 +1,5 @@
+str1 = "Thura"
+list1 = ["Thura","Ko Min Thant","Ko Toe Tat"]
+
+for item in list1:
+    print(item)
---
+++ Day6/myclass.py
@@ -0,0 +1,19 @@
+student1 = {
+    "name" : "Mg Mg",
+    "age"  : 19
+}
+
+student2 = {
+    "name" : "Ag Ag",
+    "age"  : 18
+}
+
+student3 = {
+    "name" : "Ky Ky",
+    "age"  : 21
+}
+
+classroom = [student1,student2,student3]
+
+for student in classroom:
+    print(student["name"])
