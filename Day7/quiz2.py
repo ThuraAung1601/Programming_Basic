63908774ce4a8c5e21a5da0c31ea4b3b53e0395d
Thura Aung <thuraaung1601mandalay@gmail.com>
2020-10-24 12:56:00 +0630

    Add files via upload

---
+++ Day7/quiz2.py
@@ -0,0 +1,59 @@
+'''
+quiz2.py
+Author : Thura Aung
+'''
+
+people = True
+count = 0
+party_people = []
+party_contact = []
+ages = []
+
+while people:
+        ans = input("Will you join us tonight? Y for Yes N for No: ")
+        if ans == "Y" or ans == "y":
+            count += 1
+            name = input("Your name please: ")
+            party_people.append(name)
+            ph = input("Your Ph no please: ")
+            party_contact.append(ph)
+        else: 
+            pass
+        res = input("Next One ? ")
+        if res == "Y" or res == "y":
+            people = True
+        else:
+            people = False
+print(count," people are interested in coming our party !! ")
+
+def ask_age(a):
+    if a >= 18:
+        ages.append(a)
+        return True
+    elif a >= 16:
+        check = input("Do your parents know? Y for yes N for No")
+        if check == "Y" or check == "y":
+            ages.append(a)
+            return True
+        else: 
+            del party_people[i]
+            del party_contact[i]
+            return False
+    else:
+        del party_people[i]
+        del party_contact[i]
+        return False
+
+for i in range(count):
+    print("Hello {}".format(party_people[i]))
+    a = eval(input("How old are you ?:"))
+    if ask_age(a) == True:
+        print("Welcome to the party")
+    else:
+        count -= 1
+        print("Sorry not allowed")
+    
+party_info1 = zip(party_contact,ages)
+party_full_info = dict(zip(party_people,party_info1))
+print(party_full_info)
+
